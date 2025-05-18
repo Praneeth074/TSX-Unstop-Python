e9ce66489cc4b6d9e492af4c2319e8b99a9d54c3
PraneethReddy91 <saipraneethreddy2004@gmail.com>
2025-05-18 08:30:32 +0530

    Internship

---
+++ Python assement no 2 (topic 2)/area_perimeter.py
@@ -0,0 +1,8 @@
+# Define rectangle dimensions
+length = 10
+width = 5
+# Calculate area and perimeter
+area = length * width
+perimeter = 2 * (length + width)
+print(f"Area of the rectangle: {area}")
+print(f"Perimeter of the rectangle: {perimeter}")
---
+++ Python assessment no2(topic 1)/main.py
@@ -0,0 +1,10 @@
+# hello.py
+
+# This script prints the user name with greeting message.
+
+# The print() function displays output to the console
+name = input()
+print(f"Hello, Welcome {name}!") #this is f-string formats
+
+
+# You can run this file using: python hello.py
